0bf3bace51ca81074abf60636d8860f0919b5a93
Brian Chen <enderchen004@outlook.com>
2025-05-12 09:13:34 +0100

    chore: Add copyright notices & update dependencies

--- src/routes/concept2.rs
+++ src/routes/concept2.rs
@@ -1,3 +1,18 @@
+// ErgTools - Rust-based web app & CLI application to easily calculate rowing splits and analyse performance.
+// Copyright (C) 2025 Brian Chen (differental)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use actix_web::{HttpResponse, Responder, post, web::Json};
 use regex::Regex;
 use serde::Deserialize;
